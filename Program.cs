897c94a803fe8c877f63ad417dbc603e67eb44a9
Perillio <47478204+Perillio@users.noreply.github.com>
2019-08-28 19:24:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SendUDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 0;
+            Int32.TryParse(args[1], out port);
+            UdpClient udpClient = new UdpClient();
+            IPAddress ipAddress = IPAddress.Parse(args[0]);
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+
+            Byte[] sendBytes = Encoding.ASCII.GetBytes(args[2]);
+            try
+            {
+                udpClient.Send(sendBytes, sendBytes.Length, ipEndPoint);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+    }
+}
